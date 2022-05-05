3e68625012f388dab546575ab49b9946a7ec185d
CodingLegend0100 <codinglegend0100@gmail.com>
2022-05-05 12:12:20 -0400

    Initialize game window

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
+import javax.swing.JFrame;
+
 public class Main {
     public static void main(String[] args){
-        
+
+        //Initialize the game window
+        JFrame window = new JFrame("Game Window");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setVisible(true);
     }
 }
