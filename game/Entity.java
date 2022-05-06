25db7cd08158730e95c452365f8bccfd7f305a6d
CodingLegend0100 <codinglegend0100@gmail.com>
2022-05-06 08:46:54 -0400

    Made variables public

--- game/Entity.java
+++ game/Entity.java
@@ -3,8 +3,8 @@ package game;
 import java.awt.Image;
 
 public class Entity extends Sprite {
-    private double velocityX, velocityY;
-    private double rotationSpeed;
+    public double velocityX, velocityY;
+    public double rotationSpeed;
 
     public Entity(String image, double x, double y) {
         super(image, x, y);
@@ -19,16 +19,9 @@ public class Entity extends Sprite {
         super(i, x, y, width, height);
     }
 
-    public double getVelocityX(){ return velocityX; }
-    public double getVelocityY(){ return velocityY; }
-    public double getRotationSpeed(){ return rotationSpeed; }
-
-    public void setVelocityX(double velocityX){ this.velocityX = velocityX; }
-    public void setVelocityY(double velocityY){ this.velocityY = velocityY; }
-    public void setRotationSpeed(double rotationSpeed){ this.rotationSpeed = rotationSpeed; }
-
     public void update(){
-        setX(getX()+velocityX);
-        setY(getY()+velocityY);
+        x += velocityX;
+        y += velocityY;
+        rotation += rotationSpeed;
     }
 }
--- game/Sprite.java
+++ game/Sprite.java
@@ -4,13 +4,11 @@ import java.awt.Image;
 import java.awt.Graphics2D;
 import javax.swing.ImageIcon;
 
-import java.awt.Color;
-
 /** This class is intended for displaying static images onto the screen */
 public class Sprite {
-    private double x, y; //Represents the sprites CENTER x and y
-    private int width,height;
-    private int rotation; //Rotation in degrees
+    public double x, y; //Represents the sprites CENTER x and y
+    public int width,height;
+    public double rotation; //Rotation in degrees
     private Image i;
 
     /**Create a sprite whose width and height will be inherited from the image's width and height */
@@ -33,7 +31,6 @@ public class Sprite {
         this.width = width;
         this.height = height;
         setImage(i);
-        rotation = 10;
     }
 
     /**Loads the image from the string filepath */
@@ -41,30 +38,20 @@ public class Sprite {
         return new ImageIcon(imagePath).getImage();
     }
 
-    public void setX(double x){ this.x = x; }
-    public void setY(double y){ this.y = y; }
-    public void setWidth(int width){ this.width = width; }
-    public void setHeight(int height){ this.height = height; }
     public void setImage(Image i){ this.i = i; }
-
-    public double getX(){ return x; }
-    public double getY(){ return y; }
-    public int getWidth(){ return width; }
-    public int getHeight(){ return height; }
     public Image getImage(){ return i; }
 
     /** Draws the sprite */
     public void draw(Graphics2D g){
+        if (i == null) return;
 
         double rotate = Math.toRadians(rotation);
 
         g.rotate(rotate,x,y);
+        
         g.drawImage(i,(int)x-width/2,(int)y-height/2,width,height,null);
 
         g.rotate(-rotate,x,y);
-
-        g.setColor(Color.RED);
-        g.fillOval((int)x,(int)y,5,5);
 
     }
 }
