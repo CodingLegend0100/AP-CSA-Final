8891ba0d7249dfbc37845fae4e1d6c92ab428fd2
CodingLegend0100 <codinglegend0100@gmail.com>
2022-05-06 09:40:10 -0400

    Window changes

--- Main.java
+++ Main.java
@@ -1,14 +1,21 @@
+import game.Sprite;
+
 import javax.swing.JFrame;
 
 public class Main {
+
+    //Useful Links
+    //https://docs.oracle.com/javase/7/docs/api/java/awt/Graphics.html
+    //https://docs.oracle.com/javase/7/docs/api/java/awt/Graphics2D.html
     public static void main(String[] args){
 
         //Initialize the game window
-        JFrame window = new JFrame("Game Window");
+        JFrame window = new JFrame("Space Miner");
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setSize(900,600);
         window.setResizable(false);
         window.add(new GamePanel());
+        window.pack();
+        window.setIconImage(Sprite.loadImage("assets/spaceship.png"));
         window.setVisible(true);
     }
 }
