94856f04dea4d14a8978f05e5ec07a00820a12b4
CodingLegend0100 <codinglegend0100@gmail.com>
2022-05-09 16:42:27 -0700

    Code fixes

--- Asteroid.java
+++ Asteroid.java
@@ -9,15 +9,11 @@ public class Asteroid extends Sprite{
     
     private Image[] asteroidImages = new Image[3];
 
-    private ArrayList<Sprite> asteroids = new ArrayList<Sprite>();{
-
-    for (int i = 1; i < 4; i++){
-        asteroidImages[i-1] = loadImage("assets/asteroid"+i+".png");
+    {   
+        for (int i = 1; i < 4; i++){
+            asteroidImages[i-1] = loadImage("assets/asteroid"+i+".png");
+        }
     }
-    
-    asteroids.add(new Asteroid(asteroidImages[0],100,200));
-    asteroids.add(new Asteroid(asteroidImages[1],150,200));
-    asteroids.add(new Asteroid(asteroidImages[2],200,200));}
     
     public Asteroid(Image i, double x, double y){
         super(i,x,y);
