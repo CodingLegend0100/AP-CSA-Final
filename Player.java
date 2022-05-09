2a2da9951d07e0c5da65604f97ff0ebab06eae0f
CodingLegend0100 <codinglegend0100@gmail.com>
2022-05-09 11:18:57 -0400

    Comment fixes

--- Player.java
+++ Player.java
@@ -5,9 +5,8 @@ public class Player extends Sprite {
 
     private double velX = 0, velY = 0;
     private double turnSpeed = 2.5;
-    private double acceleration = 0.2;
+    private double acceleration = 0.2; //Increases max speed by 5 per 0.1
     private double glide = 0.98;
-    //private int maxSpeed = 5;
 
     public Player(KeyInput k){
         super("assets/spaceship.png",200,200,0.1);
@@ -26,19 +25,12 @@ public class Player extends Sprite {
         velY *= glide;
 
         if (k.isKeyDown("W")){
-            //Add vectors
+            //Convert direction from degrees to radians
             double rad = Math.toRadians(rotation-90); //Vector angle correction
 
-            //Create acceleration vector and add it to velocity vector
+            //Add acceleration vector to velocity vector
             velX += acceleration*Math.cos(rad);
             velY += acceleration*Math.sin(rad);
-
-
-            //double direction = Math.atan2(velY, velX); //Calculate direction of the velocity vector
-            //velocity *= 0.7; //Resistance factor
-
-            //Cap the velocity
-            //if (velocity > maxSpeed) { velocity = maxSpeed; }
         }
         if (k.isKeyDown("A")) rotation -= turnSpeed;
         if (k.isKeyDown("D")) rotation += turnSpeed;
