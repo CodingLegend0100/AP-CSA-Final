bb492e797a15f98ee0d7b79c78aadc736b0ef8da
CodingLegend0100 <codinglegend0100@gmail.com>
2022-05-11 13:32:18 -0400

    Menu update

--- Menu.java
+++ Menu.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics2D;
 
 public class Menu {
@@ -11,7 +12,12 @@ public class Menu {
     public void draw(Graphics2D g){
         if (!open) return;
 
+        g.setColor(Color.WHITE);
+        g.fillRoundRect(50,50,800,500,10,10);
         g.setColor(Color.LIGHT_GRAY);
-        g.fillRoundRect(50,50,800,500,50,50);
+        g.fillRect(60,60,780,480);
+        g.setColor(Color.BLACK);
+        g.setFont(new Font(Font.DIALOG,Font.PLAIN,20));
+        g.drawString("Space Station",400,80);
     }
 }
