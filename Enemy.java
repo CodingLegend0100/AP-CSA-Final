dba941e494291361eff1cddd17cb28c15cb7cb52
CodingLegend0100 <codinglegend0100@gmail.com>
2022-05-12 08:10:04 -0400

    Improved enemy AI

--- Enemy.java
+++ Enemy.java
@@ -7,7 +7,8 @@ public class Enemy extends Sprite {
     private double velX = 0, velY = 0;
     private double acceleration = 0.2; //Increases max speed by 5 per 0.1
     private double glide = 0.98;
-    private double followDistance = 150;
+    //private double followDistance = 150;
+    private double turn = 2.5;
 
     public Enemy(Image i, double x, double y, Sprite target){
         super(i,x,y,0.3);
@@ -15,20 +16,24 @@ public class Enemy extends Sprite {
     }
     public void update(){
         //Very basic tracking/pathfinding
-
         x += velX;
         y += velY;
 
         velX *= glide;
         velY *= glide;
 
-        double rad = (Math.atan2(target.getX()-x,y-target.getY()));
-        rotation = Math.toDegrees(rad);
+        double rad = (Math.atan2(y-target.getY(),x-target.getX()));
+        double delta = (Math.toDegrees(rad)-rotation+90)%360;
+        //System.out.println(delta);
+        if (delta > 180) rotation += turn;  //Turn right
+        else rotation += -turn; //Turn left
 
-        if (Math.pow(target.getX()-x,2)+Math.pow(target.getY()-y,2) > followDistance*followDistance){
-            //Add acceleration vector to velocity vector
-            velX += acceleration*Math.cos(rad-Math.PI/2);
-            velY += acceleration*Math.sin(rad-Math.PI/2);
-        }
+        rotation %= 360;
+
+        rad = Math.toRadians(rotation);
+
+        //Add acceleration vector to velocity vector
+        velX += acceleration*Math.cos(rad-Math.PI/2);
+        velY += acceleration*Math.sin(rad-Math.PI/2);
     }
 }
