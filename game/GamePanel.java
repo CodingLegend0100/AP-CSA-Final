a8ed6feedf8cf1037ac3641ac4e2be1aefc18c08
CodingLegend0100 <codinglegend0100@gmail.com>
2022-05-16 18:07:42 -0400

    Removed todos

--- game/GamePanel.java
+++ game/GamePanel.java
@@ -117,31 +117,26 @@ public class GamePanel extends JPanel implements Runnable {
 
         @Override
         public void mouseClicked(MouseEvent e) {
-            // TODO Auto-generated method stub
 
         }
 
         @Override
         public void mousePressed(MouseEvent e) {
-            // TODO Auto-generated method stub
             GamePanel.this.mousePressed(e.getX(),e.getY());
         }
 
         @Override
         public void mouseReleased(MouseEvent e) {
-            // TODO Auto-generated method stub
             
         }
 
         @Override
         public void mouseEntered(MouseEvent e) {
-            // TODO Auto-generated method stub
             
         }
 
         @Override
         public void mouseExited(MouseEvent e) {
-            // TODO Auto-generated method stub
             
         }
 
