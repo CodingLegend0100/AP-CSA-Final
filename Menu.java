6eb351af5b7d3d8dd12e9b82ddedaf079f1d2ab3
CodingLegend0100 <codinglegend0100@gmail.com>
2022-05-17 09:47:38 -0400

    More menu work

--- Menu.java
+++ Menu.java
@@ -19,7 +19,8 @@ public class Menu {
     Rectangle buyButton = new Rectangle(200,250,200,100);
     Rectangle sellButton = new Rectangle(500,250,200,100);
 
-    Sprite exit = new Sprite(Sprite.loadImage("assets/x.png"),75,75,20,20);
+    Sprite exit = new Sprite(Sprite.loadImage("assets/x.png"),80,80,30,30);
+    Sprite back = new Sprite(Sprite.loadImage("assets/back_arrow.png"),80,80,30,30);
 
 
     public void open(){
@@ -36,6 +37,8 @@ public class Menu {
             if (buyButton.contains(x,y)) screenID = BUY_SCREEN;
             else if (sellButton.contains(x,y)) screenID = SELL_SCREEN;
             else if (exit.contains(x,y)) close();
+        } else {
+            if (back.contains(x,y)) screenID = MENU_SCREEN;
         }
     }
 
@@ -64,15 +67,16 @@ public class Menu {
             g.setFont(new Font(Font.DIALOG,Font.PLAIN,50));
             g.drawString("Buy",centerX(buyButton)-40,centerY(buyButton)+15);
             g.drawString("Sell",centerX(sellButton)-40,centerY(sellButton)+15);
-        }
-
-        else if (screenID == BUY_SCREEN){
-            g.setColor(Color.GREEN);
-            g.fillOval(450,300,10,10);
-        }
-        else if (screenID == SELL_SCREEN){
-            g.setColor(Color.RED);
-            g.fillOval(450,300,10,10);
+        } else {
+            back.draw(g);
+            if (screenID == BUY_SCREEN){
+                g.setColor(Color.GREEN);
+                g.fillOval(450,300,10,10);
+            }
+            else if (screenID == SELL_SCREEN){
+                g.setColor(Color.RED);
+                g.fillOval(450,300,10,10);
+            }
         }
     }
 
