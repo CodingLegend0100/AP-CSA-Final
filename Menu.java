9423b937ab35cc400f7b8bb37d8a8b4aa5eea5ed
CodingLegend0100 <codinglegend0100@gmail.com>
2022-05-18 10:52:02 -0400

    Buttons :D

--- Menu.java
+++ Menu.java
@@ -2,8 +2,10 @@ import game.Sprite;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
+import java.awt.geom.Rectangle2D;
 
 public class Menu {
     //Screen dimensions 900x600
@@ -16,8 +18,27 @@ public class Menu {
     final int BUY_SCREEN = -1;
     final int SELL_SCREEN = 1;
 
-    Rectangle buyButton = new Rectangle(200,250,200,100);
-    Rectangle sellButton = new Rectangle(500,250,200,100);
+    Font font = new Font(Font.DIALOG,Font.PLAIN,50);
+    Font font1 = new Font(Font.DIALOG,Font.PLAIN,25);
+
+    Button buyButton = new Button("Buy",200,250,200,100,
+                                Color.WHITE,new Color(0,150,0),
+                                font);
+
+    Button sellButton = new Button("Sell",500,250,200,100,
+                                Color.WHITE,new Color(200,0,0),
+                                font);
+    
+    ButtonStyle sellStyle = new ButtonStyle().setFont(font1).setFontColor(Color.WHITE).setBackground(Color.GRAY);
+    Button sellHydrogen = new Button("Hydrogen",110,90,150,50,sellStyle);
+    Button sellLithium = new Button("Lithium",110,150,150,50,sellStyle);
+    Button sellIron = new Button("Iron",110,210,150,50,sellStyle);
+    Button sellGold = new Button("Gold",110,270,150,50,sellStyle);
+    Button sellOsmium = new Button("Osmium",110,330,150,50,sellStyle);
+    Button sell1 = new Button("Sell",110,390,150,50,sellStyle);
+    Button sell2 = new Button("Sell",110,450,150,50,sellStyle);
+    Button[] sellButtons = {sellHydrogen,sellLithium,sellIron,sellGold,sellOsmium,sell1,sell2};
+
 
     Sprite exit = new Sprite(Sprite.loadImage("assets/x.png"),80,80,30,30);
     Sprite back = new Sprite(Sprite.loadImage("assets/back_arrow.png"),80,80,30,30);
@@ -51,22 +72,13 @@ public class Menu {
         g.setColor(Color.LIGHT_GRAY);
         g.fillRect(60,60,780,480);
         g.setColor(Color.BLACK);
-        g.setFont(new Font(Font.DIALOG,Font.PLAIN,20));
-        g.drawString("Space Station",400,80);
+        g.setFont(font);
+        drawCentered(g,"Space Station",450,90);
 
         if (screenID == MENU_SCREEN){
             exit.draw(g);
-
-            g.setColor(new Color(0,150,0));
-            g.fill(buyButton);
-
-            g.setColor(new Color(200,0,0));
-            g.fill(sellButton);
-
-            g.setColor(Color.WHITE);
-            g.setFont(new Font(Font.DIALOG,Font.PLAIN,50));
-            g.drawString("Buy",centerX(buyButton)-40,centerY(buyButton)+15);
-            g.drawString("Sell",centerX(sellButton)-40,centerY(sellButton)+15);
+            buyButton.draw(g);
+            sellButton.draw(g);
         } else {
             back.draw(g);
             if (screenID == BUY_SCREEN){
@@ -74,17 +86,88 @@ public class Menu {
                 g.fillOval(450,300,10,10);
             }
             else if (screenID == SELL_SCREEN){
-                g.setColor(Color.RED);
-                g.fillOval(450,300,10,10);
+                g.setColor(Color.DARK_GRAY);
+                for (Button b : sellButtons){
+                    b.draw(g);
+                }
             }
         }
     }
 
-    private int centerX(Rectangle r){
-        return r.x+r.width/2;
+    private void drawCentered(Graphics2D g,String text, int x, int y){
+        FontMetrics fm = g.getFontMetrics();
+        Rectangle2D r = fm.getStringBounds(text, g);
+        int X = x-((int) r.getWidth()) / 2;
+        int Y = y-((int) r.getHeight()) / 2 + fm.getAscent();
+        g.drawString(text, X, Y);
     }
 
-    private int centerY(Rectangle r){
-        return r.y+r.height/2;
+    public class Button extends Rectangle {
+        Font font;
+        String text;
+        Color bg;
+        Color tc;
+        
+        public Button(String msg, int x, int y, int width, int height, ButtonStyle style){
+            super(x,y,width,height);
+            text = msg;
+            if (style != null){
+                font = style.f;
+                tc = style.fontColor;
+                bg = style.background;
+            }
+        }
+
+        public Button(String msg, int x, int y, int width, int height,Color textColor,Color background,Font f){
+            super(x,y,width,height);
+            text = msg;
+            bg = background;
+            tc = textColor;
+            font = f;
+        }
+
+        public void draw(Graphics2D g){
+            if (bg != null) g.setColor(bg);
+            
+            g.fill(this);
+
+            if (font != null) g.setFont(font);
+            if (tc != null) g.setColor(tc);
+
+
+            FontMetrics fm = g.getFontMetrics();
+            Rectangle2D r = fm.getStringBounds(text, g);
+            int x = this.x+(this.width - (int) r.getWidth()) / 2;
+            int y = this.y+(this.height - (int) r.getHeight()) / 2 + fm.getAscent();
+            g.drawString(text, x, y);
+        }
+    }
+
+    public class ButtonStyle {
+        Font f;
+        Color fontColor;
+        Color background;
+        Color border;
+        int borderWidth;
+        public ButtonStyle setFont(Font f){
+            this.f = f;
+            return this;
+        }
+        public ButtonStyle setFontColor(Color c){
+            fontColor = c;
+            return this;
+        }
+        public ButtonStyle setBackground(Color c){
+            background = c;
+            return this;
+        }
+        public ButtonStyle setBorderColor(Color c){
+            border = c;
+            return this;
+        }
+        public ButtonStyle setBorderWidth(int w){
+            borderWidth = w;
+            return this;
+        }
     }
 }
